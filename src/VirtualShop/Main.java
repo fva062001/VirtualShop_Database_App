4b8e68c152761b5612443863988bc9d769f0fced
Fernando Valerio <fernandoarturo06@gmail.com>
2022-03-24 10:50:05 -0400

    Updating repo

---
+++ src/VirtualShop/Main.java
@@ -0,0 +1,15 @@
+package VirtualShop;
+
+import VirtualShop.central;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //Fernando Valerio
+        //10138798
+
+        central myLog = new central();
+        myLog.setVisible(true);
+
+    }
+}
